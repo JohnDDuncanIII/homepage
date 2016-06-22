91ab53ce0cb27d814f1211f5ac32215ab9fe1387
JohnDDuncanIII <duncjo01@gettysburg.edu>
2016-06-21 23:02:43 -0400

    add basic support for most html/markdown features

--- assets/writings/publish.sh
+++ assets/writings/publish.sh
@@ -3,31 +3,40 @@
 # Example usage: ./publish.sh "Header Title" inputFile.txt
 # Created by John D. Duncan, III
 COUNTER_FILE="counter.txt";
-count=`cat $COUNTER_FILE`; # get the number
-FILENAME=$count; # first part of the filename is a number that gets incremented each time you post
-FILENAME="$FILENAME`echo _$1 | awk '{print $1;}'| tr '[:upper:]' '[:lower:]'`"; # make filename the first word of the title
-FILENAME="$FILENAME.html"; # make it an html document
+count=`cat $COUNTER_FILE`; # get the post number
+FILENAME="$count`echo _$1 | awk '{print $1;}'| tr '[:upper:]' '[:lower:]'`"; # make filename post #, then first word of the title
+FILENAME="$FILENAME.html";
 echo "$FILENAME";
-sed "s/\(<time>\)/\1$(date +'%A, %B %d %Y - %I:%M:%S %p')/" template.html > $FILENAME ; # add date to the footer
-sed -i "s/\(<h1>\)/\1$1/" $FILENAME; # add header title
-INFILE="`sed 's/$/<br>/' $2`" # replace newline with <br>
-INFILE="$(eval "echo \"$INFILE\"" | sed -e 's/\t/\&emsp;\&emsp;\&emsp;/g')"; # replace tab with &emsp;
-INFILE="$(echo "$INFILE" | sed 's/&/\\&/g')"; # ampersand fixes
-INFILE=$(echo "$INFILE" | sed "s/>\(.*\)\.<br>/<blockquote>\1\. <\/blockquote>/g"); # replace > .* \.<br> with blockquote
-INFILE=$(echo "$INFILE" | sed "s/\*\*\([a-zA-Z0-9[:space:]_]*\)\*\*/<b>\1<\/b>/g"); # replace ** with <b>
-INFILE=$(echo "$INFILE" | sed "s/\~\~\([a-zA-Z0-9[:space:]_]*\)\~\~/<s>\1<\/s>/g"); # replace ~~ with <s>
-INFILE=$(echo "$INFILE" | sed "s/\_\([a-zA-Z0-9[:space:]_]*\)\_/<i>\1<\/i>/g"); # replace _ with <i>
 
-INFILE="$(echo "$INFILE" | sed -e 's/\!\[\(.*\)\](\(.*\))/<img src=\2 alt=\1>/g')"; # markdown url
-INFILE="$(echo "$INFILE" | sed -e 's/\[\(.*\)\](\(.*\))/<a href=\2>\1<\/a>/g')"; # markdown url
-INFILE="$(echo "$INFILE" | sed -e "s|[[:space:]]\(http[:]//[^ ]*[a-zA-Z]\)[[:space:]]| <a href=\"\1\">\1</a> |g")"; # replace urls with html urls
-INFILE="$(echo "$INFILE" | sed -r "s|\w+@\w+\.\w+(\.\w+)?|<a href=\"mailto:\0\">\0</a>|g")"; # replace email with html mailto link
-INFILE="$(echo "$INFILE" | sed 's/\//\\\//g')"; # backslach fixes
-sed -i "s/\(<p>\)/\1$(echo $INFILE)/" $FILENAME; # add it all back into the new document
-chmod 755 $FILENAME;
-count=$((count+1))
-echo $count > $COUNTER_FILE;
+INFILE=$(sed -re '
+s/$/<br>/; # replace newline with <br>
+s/\t/\&emsp;\&emsp;\&emsp;/g; 
+s/&/\\&/g; # ampersand fixes
+s/>>(.*)\.<br>/<\/p><blockquote>\1\. <\/blockquote><p>/g;  # replace > .* \.<br> with blockquote
+s/\*\*([a-zA-Z0-9[:space:]_]*)\*\*/<b>\1<\/b>/g;  # replace ** with <b>
+s/\~\~([a-zA-Z0-9[:space:]_]*)\~\~/<s>\1<\/s>/g;  # replace ~~ with <s>
+s/```(.*)```/<code>\1<\/code>/g;  # replace ``` with <code>
+s/---/<hr>/g;  # replace ``` with <code>
+s/\_\_([a-zA-Z0-9[:space:]_]*)\_\_/<small>\1<\/small>/g;  # replace __ with <small>
+s/\#\#([a-zA-Z0-9[:space:]_]*)\#\#/<u>\1<\/u>/g;  # replace __ with <u>
+s/\%\%(.*)\%\%/<mark>\1<\/mark>/g;  # replace %% with <mark>
+s/\_\{([a-zA-Z0-9[:space:]_,]*)\}/<sub>\1<\/sub>/g;  # replace _{} with <sub>
+s/\^\{([a-zA-Z0-9[:space:]_,]*)\}/<sup>\1<\/sup>/g;  # replace ^{} with <sup>
+s/\_([a-zA-Z0-9[:space:]_]*)\_/<i>\1<\/i>/g; # replace _ with <i>
+s/\!\[(.*)\]\((.*)\)/<img src=\2 alt=\1>/g;  # markdown url (force unwrap image)
+s/\[(.*)\]\((.*)\)/<a href=\2>\1<\/a>/g;  # markdown url
+s/\[(.*)\]<([a-zA-Z0-9[:space:]_,]*)>/<abbr title=\2>\1<\/abbr>/g;  # replace []<> with <abbr>
+s|[[:space:]](http[:]//[^ ]*[a-zA-Z])[[:space:]]| <a href=\"\1\">\1</a> |g; # replace urls with html urls
+s|\w+@\w+\.\w+(\.\w+)?|<a href=\"mailto:\0\">\0</a>|g;s/\//\\\//g' $2); # replace tab with &emsp;
+
+sed "s/\(<h1>\)/\1$1/; # add header title
+s/\(<time>\)/\1$(date +'%A, %B %d %Y - %I:%M:%S %p')/ # add date to footer
+s/\(<p>\)/\1$(echo $INFILE)/" template.html > $FILENAME; # add it all back into the new document
+
+chmod 644 $FILENAME; # make readable
+#count= # increment post counter
+echo $((count+1)) > $COUNTER_FILE; # write this out to the file
 
 TABLEROW=$(echo "<TR>?<TD><a href="$FILENAME">$1<\/a><\/TD>?<TD>$(date +"%A, %B %d %Y - %I:%M:%S %p")<\/TD>?<\/TR>" | sed -e $'s/?/\\\n/g'); # add the post to the index's table
-sed -i "/\(<table\)/a $TABLEROW" index.html;
-sed -i "s/<time>\(.*\)<\/time>/<time>Modified: $(date +'%A, %B %d %Y - %I:%M:%S %p')<\/time>/" index.html; # update timestamp
+#sed -i "" index.html;
+sed -i "s/<time>\(.*\)<\/time>/<time>Modified: $(date +'%A, %B %d %Y - %I:%M:%S %p')<\/time>/;/\(<table\)/a $TABLEROW" index.html; # update timestamp
