39a763e3740840eab8199fe53dd6d5e672916799
JohnDDuncanIII <duncjo01@gettysburg.edu>
2016-06-27 19:34:32 -0400

    add table support to blogging script, some other stuff

--- assets/writings/publish.sh
+++ assets/writings/publish.sh
@@ -16,7 +16,7 @@ s/>>(.*)\.<br>/<\/p><blockquote>\1\. <\/blockquote><p>/g;  # replace > .* \.<br>
 s/\*\*([a-zA-Z0-9[:space:]_]*)\*\*/<b>\1<\/b>/g;  # replace ** with <b>
 s/\~\~([a-zA-Z0-9[:space:]_]*)\~\~/<s>\1<\/s>/g;  # replace ~~ with <s>
 s/```(.*)```/<code>\1<\/code>/g;  # replace ``` with <code>
-s/---/<hr>/g;  # replace ``` with <code>
+s/---/<\/p><hr><p>/g;  # replace --- with <hr>
 s/\_\_([a-zA-Z0-9[:space:]_]*)\_\_/<small>\1<\/small>/g;  # replace __ with <small>
 s/\#\#([a-zA-Z0-9[:space:]_]*)\#\#/<u>\1<\/u>/g;  # replace __ with <u>
 s/\%\%(.*)\%\%/<mark>\1<\/mark>/g;  # replace %% with <mark>
@@ -32,11 +32,13 @@ s|\w+@\w+\.\w+(\.\w+)?|<a href=\"mailto:\0\">\0</a>|g;s/\//\\\//g' $2); # replac
 sed "s/\(<h1>\)/\1$1/; # add header title
 s/\(<time>\)/\1$(date +'%A, %B %d %Y - %I:%M:%S %p')/ # add date to footer
 s/\(<p>\)/\1$(echo $INFILE)/" template.html > $FILENAME; # add it all back into the new document
-
+sed -i 's/<br>/<br>\n/g' $FILENAME; # physically break newlines to easily match tables
+sed -i 's/|\([a-zA-Z0-9[:space:]_,$\*\/<>-]*\)*/<td>\1<\/td>/g; s/<td><br><\/td>/<br>/g;s/\(<td>.*<\/td>\)<br>/<tr>\1<\/tr>/g' $FILENAME; # match markdown-extended formatted tables: | col 1 | col 2 | etc... |
+sed -n '1h; 1!H; ${g; s/\(<tr>.*<\/tr>\)/<\/p><table>\0<\/table><p>/g p}' $FILENAME > $FILENAME.tmp && mv $FILENAME.tmp $FILENAME; # insert the table tags around the entire inserted table block -- see http://www.refining-linux.org/archives/27/20-Multi-line-sed-search-and-replace/ for explanation
+emacs -batch $FILENAME --eval '(indent-region (point-min) (point-max) nil)' -f save-buffer # LISP to properly indent the inserted HTML
+rm $FILENAME~;
 chmod 644 $FILENAME; # make readable
-#count= # increment post counter
 echo $((count+1)) > $COUNTER_FILE; # write this out to the file
 
 TABLEROW=$(echo "<TR>?<TD><a href="$FILENAME">$1<\/a><\/TD>?<TD>$(date +"%A, %B %d %Y - %I:%M:%S %p")<\/TD>?<\/TR>" | sed -e $'s/?/\\\n/g'); # add the post to the index's table
-#sed -i "" index.html;
 sed -i "s/<time>\(.*\)<\/time>/<time>Modified: $(date +'%A, %B %d %Y - %I:%M:%S %p')<\/time>/;/\(<table\)/a $TABLEROW" index.html; # update timestamp
